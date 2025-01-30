1e78c29ad03410d0d8e745e748dcc6ef96bfa91a
Adrian Mena Ruiz <adrianmenaruiz@gmail.com>
2025-01-30 18:28:02 +0100

    Added more examples

--- public/js/tienda.js
+++ public/js/tienda.js
@@ -58,30 +58,30 @@ class Producto {
 //Subclases
 
 class JetGrande extends Producto {
-    #tamano_m;
-    constructor(nombre, precio, descripcion, imagen, tamano_m) {
+    #num_pasajeros;
+    constructor(nombre, precio, descripcion, imagen, num_pasajeros) {
         super(nombre, precio, descripcion, imagen);
-        this.#tamano_m = tamano_m;
+        this.#num_pasajeros = num_pasajeros;
     }
-    get tamano_m() {
-        return this.#tamano_m;
+    get num_pasajeros() {
+        return this.#num_pasajeros;
     }
-    set tamano_m(tamano) {
-        this.#tamano_m = tamano;
+    set num_pasajeros(num) {
+        this.#num_pasajeros = num;
     }
 }
 
 class JetMediano extends Producto {
-    #alcance_km;
-    constructor(nombre, precio, descripcion, imagen, alcance_km) {
+    #num_pasajeros;
+    constructor(nombre, precio, descripcion, imagen, num_pasajeros) {
         super(nombre, precio, descripcion, imagen);
-        this.#alcance_km = alcance_km;
+        this.#num_pasajeros = num_pasajeros;
     }
-    get alcance_km() {
-        return this.#alcance_km;
+    get num_pasajeros() {
+        return this.#num_pasajeros;
     }
-    set alcance_km(alcance) {
-        this.#alcance_km = alcance;
+    set num_pasajeros(num) {
+        this.#num_pasajeros = num;
     }
 }
 
@@ -100,16 +100,16 @@ class JetPequeno extends Producto {
 }
 
 class Avioneta extends Producto {
-    #personal_cabina;
-    constructor(nombre, precio, descripcion, imagen, personal_cabina) {
+    #alcance;
+    constructor(nombre, precio, descripcion, imagen, alcance) {
         super(nombre, precio, descripcion, imagen);
-        this.#personal_cabina = personal_cabina;
+        this.#alcance = alcance;
     }
-    get personal_cabina() {
-        return this.#personal_cabina;
+    get alcance() {
+        return this.#alcance;
     }
-    set personal_cabina(personal) {
-        this.#personal_cabina = personal;
+    set alcance(personal) {
+        this.#alcance = personal;
     }
 }
 
@@ -128,30 +128,42 @@ class Helicoptero extends Producto {
 }
 
 const productos = [
-    //Jets Grandes
-    new JetGrande("Gulfstream G650", 55000, "Jet de largo alcance y lujo", "imagenes/jets/G650.jpg", 30),
-    new JetGrande("Gulfstream G700", 70000, "Jet de última generación", "imagenes/jets/G700.jpg", 33),
-    new JetGrande("Bombardier Global 7500", 75000, "Jet intercontinental con máxima comodidad", "imagenes/jets/Global7500.jpg", 34),
+    // todos los Jets tienen como parametro adicional el numero de pasajeros.
+    // Jets Grandes. (constructora) +  Numero de pasajeros.
+    new JetGrande("Gulfstream GIV-SP", 45000, "Modelo clásico dentro del rango máximo", "imagenes/jets/Gulfstream GIV-SP.jpg", 14),
+    new JetGrande("Gulfstream G600", 55000, "Capacidad máxima y buena relacion calidad/precio", "imagenes/jets/Gulfstream G600.jpg", 18),
+    new JetGrande("Gulfstream G650", 60000, "Jet de largo alcance y lujo", "imagenes/jets/Gulfstream G650.jpg", 19),
+    new JetGrande("Gulfstream G700", 70000, "Jet de última generación", "imagenes/jets/Gulfstream G700.jpg", 20),
+    new JetGrande("Bombardier Global 7500", 75000, "Jet intercontinental con máxima comodidad", "imagenes/jets/Bombardier Global 7500.jpg", 19),
+    new JetGrande("Bombardier Global 8000", 80000, "Gama premium, intercontinental, moderno y de extra lujo", "imagenes/jets/Bombardier Global 8000.jpg", 22),
 
-    //Jets Medianos
-    new JetMediano("Gulfstream G500", 45000, "Jet ejecutivo de rango medio", "imagenes/jets/G500.jpg", 9630),
-    new JetMediano("Cessna Citation X+", 35000, "Jet mediano con gran velocidad", "imagenes/jets/CitationX.jpg", 5956),
-    new JetMediano("Embraer Legacy 500", 32000, "Jet con avanzada tecnología de cabina", "imagenes/jets/Legacy500.jpg", 5744),
+    // Jets Medianos. (constructora) +  Numero de pasajeros.
+    new JetMediano("Gulfstream G500", 45000, "Jet ejecutivo de rango medio", "imagenes/jets/Gulfstream G650.jpg", 16),
+    new JetMediano("Cessna Citation X+", 35000, "Jet mediano con gran velocidad", "imagenes/jets/Cessna Citation X+.jpg", 12),
+    new JetMediano("Cessna Citation Longitude", 37000, "Gran alcance para su precio", "imagenes/jets/Cessna Citation Longitude.jpg", 12),
+    new JetMediano("Embraer Legacy 500", 32000, "Jet con avanzada tecnología de cabina", "imagenes/jets/Embraer Legacy 500.jpg", 12),
+    new JetMediano("Bombardier Challenger 350", 34000, "Alcance medio, perfecto para viajes ejecutivos", "imagenes/jets/Bombardier Challenger 350.jpg", 10),
 
-    //Jets Pequeños
-    new JetPequeno("Cessna Citation CJ4", 28000, "Jet ligero con gran versatilidad", "imagenes/jets/CJ4.jpg", 10),
-    new JetPequeno("Honda HondaJet Elite", 25000, "Jet pequeño con alta eficiencia", "imagenes/jets/HondaJetElite.jpg", 6),
-    new JetPequeno("Bombardier Learjet 75 Liberty", 28000, "Jet compacto y rápido", "imagenes/jets/Learjet75.jpg", 9),
+    // Jets Pequeños. (constructora) +  Numero de pasajeros.
+    new JetPequeno("Cessna Citation CJ4", 28000, "Mucha cantidad de pasajeros", "imagenes/jets/Cessna Citation CJ4.jpg", 10),
+    new JetPequeno("Cessna Citation CJ3+", 28000, "Moderno y con muy buena eficiencia de combustible", "imagenes/jets/Cessna Citation CJ3+.jpg", 9),
+    new JetPequeno("Cessna Citation M2", 18000, "Jet compacto y económico en mantenimiento y uso", "imagenes/jets/Cessna Citation M2.jpg", 6),
+    new JetPequeno("Honda HondaJet Elite", 25000, "Jet pequeño con alta eficiencia", "imagenes/jets/Honda HondaJet Elite.jpg", 6),
+    new JetPequeno("Bombardier Learjet 75 Liberty", 28000, "Jet compacto y rápido", "imagenes/jets/Bombardier Learjet 75 Liberty.jpg", 9),
 
-    //Avionetas
-    new Avioneta("Cessna 172 Skyhawk", 5000, "Avioneta de entrenamiento y turismo", "imagenes/jets/C172.jpg", 0),
-    new Avioneta("Piper PA-28 Cherokee", 5500, "Avioneta versátil para viajes cortos", "imagenes/jets/PA28.jpg", 0),
-    new Avioneta("Diamond DA40", 4000, "Avioneta ligera y eficiente", "imagenes/jets/DA40.jpg", 0),
+    // Avionetas. (constructora) +  Alcance en KMs.
+    new Avioneta("Cessna 172 Skyhawk", 5000, "Avioneta de entrenamiento y turismo", "imagenes/jets/Cessna 172 Skyhawk.jpg", 1289),
+    new Avioneta("Piper PA-28 Cherokee", 5500, "Avioneta versátil para viajes cortos", "imagenes/jets/Piper PA-28 Cherokee.jpg", 1389),
+    new Avioneta("Diamond DA40", 4000, "Avioneta ligera y eficiente", "imagenes/jets/Diamond DA40.jpg", 1350),
+    new Avioneta("Beechcraft Bonanza G36", 6000, "Avioneta premium, para los más exigentes", "imagenes/jets/Beechcraft Bonanza G36jpg", 1482),
+    new Avioneta("Mooney M20", 4500, "Avioneta con gran alcance para su precio", "imagenes/jets/Mooney M20.jpg", 1600),
 
-    //Helicópteros
-    new Helicoptero("Robinson R44", 4000, "Helicóptero ligero de entrenamiento", "imagenes/jets/R44.jpg", ["Wi-Fi"]),
-    new Helicoptero("Bell 407", 7000, "Helicóptero de lujo con gran capacidad", "imagenes/jets/Bell407.jpg", ["Wi-Fi"]),
-    new Helicoptero("Airbus H125", 8000, "Helicóptero con alto rendimiento", "imagenes/jets/H125.jpg", ["Wi-Fi"])
+    // Helicópteros. (constructora) +  Facilidades.
+    new Helicoptero("Robinson R44", 4000, "Helicóptero ligero de entrenamiento", "imagenes/jets/Robinson R44.jpg", ["Wi-Fi"]),
+    new Helicoptero("Bell 407", 7000, "Helicóptero de lujo con gran capacidad", "imagenes/jets/Bell 407.jpg", ["Wi-Fi", "Cámara profesional"]),
+    new Helicoptero("AgustaWestland AW109", 12000, "Helicóptero de rescate y operaciones especiales", "imagenes/jets/AgustaWestland AW109.jpg", ["Wi-Fi"]),
+    new Helicoptero("Sikorsky S-76", 15000, "Helicóptero para transporte de material pesado", "imagenes/jets/Sikorsky S-76.jpg", ["Wi-Fi"]),
+    new Helicoptero("Airbus H125", 8000, "Helicóptero con alto rendimiento", "imagenes/jets/Airbus H125.jpg", ["Wi-Fi"])
 ];
 
 //Lista para el carrito
