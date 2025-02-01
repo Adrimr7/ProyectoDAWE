73b77d98988c0f33f8398f13113105d02d326da7
Jon <jonmarcos1@gmail.com>
2025-02-01 17:32:39 +0100

    search bar almost finished

--- public/js/utils.js
+++ public/js/utils.js
@@ -3,4 +3,15 @@ export function guidGenerator() {
     return `${S4()}${S4()}-${S4()}-${S4()}-${S4()}-${S4()}${S4()}${S4()}`;
 }
 
+export function actualizarTitulo() {
+    let input = document.getElementById("buscador").value.trim();
+    let titulo = document.getElementById("titulo");
+
+    if (input === "") {
+        titulo.textContent = "Todos los productos";
+    } else {
+        titulo.textContent = `Buscando por: ${input}`;
+    }
+}
+
 export const IMAGEN_POR_DEFECTO = "imagenes/default.png";
