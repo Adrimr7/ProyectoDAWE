81841d65c6b751532adc83f594aae7e53afeca48
jmm <jonmarcos1@gmail.com>
2025-03-18 09:58:43 +0100

    update package versions

---
+++ nimbus_aviation/src/serviceWorkerRegistration.js
@@ -0,0 +1,24 @@
+export function register(config) {
+    if ('serviceWorker' in navigator) {
+      window.addEventListener('load', () => {
+        const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
+  
+        navigator.serviceWorker
+          .register(swUrl)
+          .then((registration) => {
+            console.log('Service Worker registrado con éxito:', registration);
+          })
+          .catch((error) => {
+            console.error('Error al registrar el Service Worker:', error);
+          });
+      });
+    }
+  }
+  
+  export function unregister() {
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker.ready.then((registration) => {
+        registration.unregister();
+      });
+    }
+  }
